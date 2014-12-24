4a256d38f97b3e82afe25a9c7dcfd735e0c783b4
Luiz Guilherme da Silva Junior <luizguilherme@recebedor.com>
2014-12-24 01:09:19 -0200

    Making apresentation better for Dafiti presentation

--- jqscript.js
+++ jqscript.js
@@ -1,36 +1,162 @@
+window.dft = {};
 
+
+// Módulos sempre são stricts, auto invocáveis e expostos via jQuery Plugin
 (function(){
 
-    var Class = function() {
+    'use strict';
+
+    //self called
+    //$.fn.module = ...
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(function($){
+
+    'use strict';
+
+    var Say = function(element) {
+        this.$element = $(element);
         this.hello = 'hello';
-    }
+        console.log("SAY CONSTRUIDO");
 
-    Class.prototype.world = function() {
+
+        $(document).one('click', '.say-hello', $.proxy(this, 'sayHello'));
+    };
+
+    Say.prototype.getWorld = function() {
         return 'world';
-    }
+    };
 
-    Class.prototype.sayHello = function () {
-        $('body').html(this.hello + ' ' + this.world() + "<br><br><br><br><br>JQuery!");
-        $(document).trigger('sup.JQuery');
-    }
+    Say.prototype.sayHello = function () {
+        this.$element.html(this.hello + ' ' + this.getWorld());
+    };
 
-    $(document).on('keypress', function(){
-        console.log('oi');
-        var obj = new Class();
-        obj.sayHello();
+
+    $.fn.say = function() {
+      return this.each(function() {
+        var $this   = $(this);
+        var data    = $this.data('dft.say');
+
+        if (!data) $this.data('dft.say', (data = new Say(this)));
+      });
+    };
+
+    $(document).on('click', '.say-hello', function(){
+        $('.sayer').say();
     });
-})();
+
+})(jQuery);
 
 
-(function(){
-    $(document).on('sup.JQuery', function(){
-        $('body').css('background-color','red');
-        $('body').css('color','white');
-        $(document).one('sup.simple', function(){
-            $('body').css('color','black');
-        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(function($){
+
+    'use strict';
+
+    var Say2 = function(element, options) {
+        this.$element = $(element);
+        this.options = options;
+
+        this.hello = 'hello';
+        console.log("SAY 2 CONSTRUIDO");
+    };
+
+    Say2.DEFAULTS = {
+      prefix: 'Load event -->>>>'
+    };
+
+    Say2.prototype.getWorld = function() {
+        return 'world';
+    };
+
+    Say2.prototype.sayHello = function () {
+        this.$element.html(this.options.prefix + ' ' + this.hello + ' ' + this.getWorld());
+    };
+
+    Say2.prototype.say = function(text) {
+        this.$element.html(this.options.prefix + ' ' + text);
+    };
+
+
+    $.fn.say2 = function(option, args) {
+      return this.each(function() {
+        var $this   = $(this);
+        var data    = $this.data('dft.say2');
+        var options = $.extend(
+          {},
+          Say2.DEFAULTS,
+          $this.data(),
+          typeof option == 'object' && option
+        );
+
+        if (!data) $this.data('dft.say2', (data = new Say2(this, options)));
+        if (typeof option == 'string') data[option](args);
+        else if (options.show) data.open();
+      });
+    };
+
+    $(document).on('dft.say2.load', function(){
+        $('.sayer').say2('sayHello');
     });
-})();
+
+})(jQuery);
 
 
 
@@ -40,36 +166,26 @@
 
 
 
+(function($) {
 
+    'use strict';
 
+    var SpeakerAction = function() {};
 
+    SpeakerAction.prototype.run = function() {
+      this.initSay2();
+    };
 
+    SpeakerAction.prototype.initSay2 = function() {
+      $(document).trigger('dft.say2.load');
+    };
 
+    $(document).ready(function(){
+      if ($('body').data('route') == 'speaker') {
+        (new SpeakerAction()).run();
+      }
+    });
 
+    window.dft.SpeakerAction = new SpeakerAction();
 
-
-
-
-/*
-// this does the DOM search for $('.container') "n" times
-for (var i = 0; i < n; i++) {
-    $('.container').append("Line "+i+"<br />");
-}
-
-// this accomplishes the same thing...
-// but only does the DOM search for $('.container') once,
-// although it does still modify the DOM "n" times
-var $container = $('.container');
-for (var i = 0; i < n; i++) {
-    $container.append("Line "+i+"<br />");
-}
-
-// or even better yet...
-// this version only does the DOM search for $('.container') once
-// AND only modifies the DOM once
-var $html = '';
-for (var i = 0; i < n; i++) {
-    $html += 'Line ' + i + '<br />';
-}
-$('.container').append($html);
-*/
+})(jQuery);
